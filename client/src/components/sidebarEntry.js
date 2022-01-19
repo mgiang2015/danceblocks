df0949e4c327186472ffb9d99877462ef973a41a
Le Minh Giang <mgiang2015@gmail.com>
2022-01-19 23:38:29 +0800

    Allow users to change color and label

--- client/src/components/sidebarEntry.js
+++ client/src/components/sidebarEntry.js
@@ -1,20 +1,37 @@
 import { useState } from "react"
+import { changeCircle } from "../slices/circlesSlice"
+import { useDispatch } from 'react-redux'
 
 const SidebarEntry = ({ id, title, backgroundColor }) => {
     const [editable, setEditable] = useState(false)
     const [currTitle, setCurrTitle] = useState("")
     const [currColor, setCurrColor] = useState("")
+    const dispatch = useDispatch()
 
     const toggleEditable = () => {
+        setCurrColor("")
+        setCurrTitle("")
         setEditable(!editable)
+    }
+
+    const submitChanges = () => {
+        // dispatch all the changes
+        const dispatchTitle = currTitle !== "" ? currTitle : title
+        const dispatchBackgroundColor = currColor !== "" ? currColor : backgroundColor
+        dispatch(changeCircle({
+            id: id,
+            title: dispatchTitle,
+            backgroundColor: dispatchBackgroundColor,
+        }))
+        toggleEditable()
     }
 
     if (editable) {
         return (
             <div>
-                <input placeholder="New label" />
-                <input placeholder="New Color" />
-                <button onClick={toggleEditable}>Done</button>
+                <input placeholder="New label" onChange={(event) => setCurrTitle(event.target.value)}/>
+                <input placeholder="New Color" onChange={(event) => setCurrColor(event.target.value)}/>
+                <button onClick={submitChanges}>Done</button>
             </div>
         )
     }
