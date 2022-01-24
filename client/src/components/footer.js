7ca11be6e27c833bbfa3a28801a3a37ba7f0f308
le.minhgiang <le.minhgiang@bytedance.com>
2022-01-24 12:06:14 +0800

    Add necessary links to footer

--- client/src/components/footer.js
+++ client/src/components/footer.js
@@ -4,7 +4,9 @@ import styles from './container.module.css'
 const Footer = () => {
     return (
         <div className={styles.footer}>
-            <Link href="https://github.com/mgiang2015/danceblocks">Source Code</Link>
+            <Link target="_blank" href="https://github.com/mgiang2015/danceblocks">Source Code</Link>
+            <Link target="_blank" href="https://mgiang2015.github.io/danceblocks/">User Guide</Link>
+            <Link target="_blank" href="https://forms.gle/at19GWgjgwhaVxjX8">Feedback and Suggestions (Google form)</Link>
         </div>
     )
 }
