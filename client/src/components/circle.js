73088f8b562cd268efe058abb0a9dde0994e8809
le.minhgiang <le.minhgiang@bytedance.com>
2022-01-24 09:26:32 +0800

    Improve circle UI

--- client/src/components/circle.js
+++ client/src/components/circle.js
@@ -1,6 +1,6 @@
 import { useDrag } from 'react-dnd'
 import ItemTypes from '../itemTypes';
-
+import { Typography } from '@mui/material';
 
 const Circle = ({ id, left, top, backgroundColor, title, hideSourceOnDrag, }) => {
     // wire component as a drag source!
@@ -13,22 +13,27 @@ const Circle = ({ id, left, top, backgroundColor, title, hideSourceOnDrag, }) =>
     }), [id, left, top]);
 
     // styling for circle not dragging
-    const style = {
+    const circleStyle = {
         position: 'absolute',
         backgroundColor: backgroundColor,
         width: '3em',
         height: '3em',
         borderRadius: '50%',
+    };
+
+    const circleContainerStyle = {
+        position: 'absolute',
         cursor: 'move',
-    };    
+    }
     
     if (isDragging && hideSourceOnDrag) {
         return <div ref={drag}/>;
     }
 
     return (
-        <div ref={drag} style={{ ...style, left, top }} >
-            {title}
+        <div ref={drag} style={{ ...circleContainerStyle, left, top }} >
+            <Typography>{title}</Typography>
+            <div style={{ ...circleStyle }}></div>
         </div>
     );
 };
