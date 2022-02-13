ff35d213bfa3e67b7f53ce3895ab627b814a81f2
le.minhgiang <le.minhgiang@bytedance.com>
2022-02-13 23:10:44 +0800

    Create a basic server with auth login route

---
+++ server/app.js
@@ -0,0 +1,17 @@
+const express = require('express')
+
+const authRouter = require("./routes/auth")
+
+const PORT = process.env.PORT || 5000
+
+const app = express()
+
+app.get('/', (req, res, next) => {
+    res.send('Hello World!')
+})
+
+app.use("/auth", authRouter)
+
+app.listen(PORT, () => {
+    console.log(`Server listening on PORT ${PORT}`)
+})
